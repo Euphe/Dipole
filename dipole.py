b6a9e32777c4020d8e21d090e6203cddf3d7c36f
Jacob Dixon <jacob.dixon@okstate.edu>
2017-12-24 15:51:21 -0600

    modified for upwork project

--- dipole.py
+++ dipole.py
@@ -14,63 +14,6 @@ mu0=4*pi*1e-7
 eps0=1./(mu0*c**2)
 multipleTimeSamples = False
 
-
-def Hertz_dipole (r, p, R, phi, f, t=0, epsr=1.):
-	"""
-	Calculate E and B field strength radiated by hertzian dipole(s).
-	p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
-	R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
-	r: observation point [x,y,z]
-	f: array of frequencies [f0,f1,...]
-	t: time
-	phi: array with dipole phase angles (0..2pi) [phi0,phi1,...,phin]
-	return: fields values at observation point r at time t for every frequency in f. E and B are (3 components,number of frequencies) arrays.
-	"""
-	nf = len(f)
-	rprime = r-R  # r'=r-R
-	if numpy.ndim(p) < 2:
-		magrprime = numpy.sqrt(numpy.sum((rprime)**2))
-		magrprimep = numpy.tile(magrprime, (len(f),1)).T
-		phip = numpy.tile(phi, (len(f),1))
-		w = 2*pi*f  # \omega
-		k = w/c     # wave number
-		krp = k*magrprimep  # k|r'|
-		rprime_cross_p = numpy.cross(rprime, p) # r'x p
-		rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
-		rprime_dot_p = numpy.sum(rprime*p)
-		expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-		phiOutput = numpy.exp(1j*(w*t-krp+phip.T))
-		Ex = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[0],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[0]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[0].T,(len(f),1)).T))
-		Ey = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[1],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[1]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[1].T,(len(f),1)).T))
-		Ez = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[2],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[2]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[2].T,(len(f),1)).T))
-		Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[0],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[1],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[2],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		E = numpy.vstack((Ex,Ey,Ez))
-		B = numpy.vstack((Bx,By,Bz))
-	else:
-		magrprime = numpy.sqrt(numpy.sum((rprime)**2,axis=1))
-		magrprimep = numpy.tile(magrprime, (len(f),1)).T
-		phip = numpy.tile(phi, (len(f),1))
-		fp = numpy.tile(f,(len(magrprime),1))
-		w = 2*pi*fp  # \omega
-		k = w/c     # wave number
-		krp = k*magrprimep  # k|r'|
-		rprime_cross_p = numpy.cross(rprime, p) # r' x p
-		rp_c_p_c_rp = numpy.cross(rprime_cross_p, rprime) # (r' x p) x r'
-		rprime_dot_p = numpy.sum(rprime*p,axis=1)
-		expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-		phiOutput = numpy.exp(1j*(w*t-krp+phip.T))
-		Ex = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[:,0],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,0]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,0].T,(len(f),1)).T))
-		Ey = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[:,1],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,1]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,1].T,(len(f),1)).T))
-		Ez = expfac*(w**2/(c**2*magrprimep**3) * (numpy.tile(rp_c_p_c_rp[:,2],(nf,1))).T+(1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,2]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,2].T,(len(f),1)).T))
-		Bx = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,0],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		By = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,1],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		Bz = expfac/(magrprimep**2*c**3)*(w**2*numpy.tile(rprime_cross_p[:,2],(nf,1)).T)*(1-c/(1j*w*magrprimep))
-		E = numpy.vstack((numpy.sum(Ex,axis=0),numpy.sum(Ey,axis=0),numpy.sum(Ez,axis=0)))
-		B = numpy.vstack((numpy.sum(Bx,axis=0),numpy.sum(By,axis=0),numpy.sum(Bz,axis=0)))
-	return E,B,phiOutput
-
 def Hertz_dipole_ff (r, p, R, phi, f, t=0, epsr=1.):
 	"""
 	Calculate E and B field strength radaited by hertzian dipole(s) in the far field.
@@ -126,61 +69,6 @@ def Hertz_dipole_ff (r, p, R, phi, f, t=0, epsr=1.):
 	return E,B,phiOutput
 
 
-
-def Hertz_dipole_nf (r, p, R, phi, f, t=0, epsr=1.):
-	"""
-	Calculate E and B field strength radiated by hertzian dipole(s)  in the near field.
-	p: array of dipole moments [[px0,py0,pz0],[px1,py1,pz1],...[pxn,pyn,pzn]]
-	R: array of dipole positions [[X0,Y0,Z0],[X1,Y1,Z1],...[Xn,Yn,Zn]]
-	r: observation point [x,y,z]
-	f: array of frequencies [f0,f1,...]
-	t: time
-	phi: array with dipole phase angles (0..2pi) [phi0,phi1,...,phin]
-	return: fields values at observation point r at time t for every frequency in f. E and B are (3 components,number of frequencies) arrays.
-	"""
-	nf = len(f)
-	rprime = r-R  # r'=r-R
-	if numpy.ndim(p) < 2:
-		magrprime = numpy.sqrt(numpy.sum((rprime)**2))
-		magrprimep = numpy.tile(magrprime, (len(f),1)).T
-		phip = numpy.tile(phi, (len(f),1))
-		w = 2*pi*f  # \omega
-		k = w/c     # wave number
-		krp = k*magrprimep  # k|r'|
-		rprime_cross_p = numpy.cross(rprime, p) # r'x p
-		rprime_dot_p = numpy.sum(rprime*p)
-		expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-		phiOutput = numpy.exp(1j*(w*t-krp+phip.T))
-		Ex = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[0]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[0].T,(len(f),1)).T))
-		Ey = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[1]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[1].T,(len(f),1)).T))
-		Ez = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[2]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[2].T,(len(f),1)).T))
-		Bx = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[0],(nf,1)).T)*1j
-		By = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[1],(nf,1)).T)*1j
-		Bz = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[2],(nf,1)).T)*1j
-		E = numpy.vstack((Ex,Ey,Ez))
-		B = numpy.vstack((Bx,By,Bz))
-	else:
-		magrprime = numpy.sqrt(numpy.sum((rprime)**2,axis=1)) #|r'|
-		magrprimep = numpy.tile(magrprime, (len(f),1)).T
-		phip = numpy.tile(phi, (len(f),1))
-		fp = numpy.tile(f,(len(magrprime),1))
-		w = 2*pi*fp  # \omega
-		k = w/c     # wave number
-		krp = k*magrprimep  # k|r'|
-		rprime_cross_p = numpy.cross(rprime, p) # r' x p
-		rprime_dot_p = numpy.sum(rprime*p,axis=1) # r'.p
-		expfac = numpy.exp(1j*(w*t-krp+phip.T))/(4*pi*eps0*epsr)
-		phiOutput = numpy.exp(1j*(w*t-krp+phip.T))
-		Ex = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,0]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,0].T,(len(f),1)).T))
-		Ey = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,1]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,1].T,(len(f),1)).T))
-		Ez = expfac*((1/magrprimep**3-w*1j/(c*magrprimep**2))*(numpy.tile(3*rprime[:,2]*rprime_dot_p,(len(f),1)).T/magrprimep**2-numpy.tile(p[:,2].T,(len(f),1)).T))
-		Bx = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[:,0],(nf,1)).T)*1j
-		By = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[:,1],(nf,1)).T)*1j
-		Bz = expfac/(magrprimep**3*c**2)*(w*numpy.tile(rprime_cross_p[:,2],(nf,1)).T)*1j
-		E = numpy.vstack((numpy.sum(Ex,axis=0),numpy.sum(Ey,axis=0),numpy.sum(Ez,axis=0)))
-		B = numpy.vstack((numpy.sum(Bx,axis=0),numpy.sum(By,axis=0),numpy.sum(Bz,axis=0)))
-	return E,B,phiOutput
-	
 def createDirectory():
 	folderName = time.strftime("%Y%m%d-%H%M%S")
 	directory = os.getcwd()
@@ -198,7 +86,7 @@ if __name__ == "__main__":
 	y = 2 # above dipole
 	x=numpy.linspace(-xmax,xmax,nx)
 	z=numpy.linspace(-zmax,zmax,nz)
-	observationTime =  1#obs. time (nanoseconds) used if multipleTimeSamples = False
+	observationTime =  1 #obs. time (nanoseconds) used if multipleTimeSamples = False
 
 	#dipole
 	freq=numpy.array([1000e6])
@@ -214,6 +102,7 @@ if __name__ == "__main__":
 	path = createDirectory()
 	print("Computing the radiation...")
 	
+	
 	if multipleTimeSamples == True:
 		nt=100
 		t0=1/freq/10
@@ -223,6 +112,7 @@ if __name__ == "__main__":
 		t=numpy.linspace(t0,t1,nt)
 		fig = figure(num=1,figsize=(10,6),dpi=300)
 		for k in range(nt): #t[k] is the time sample
+			startTime = time.time()
 			P=numpy.zeros((nx,nz))
 			for i in range(nx):
 				for j in range(nz):
@@ -231,9 +121,8 @@ if __name__ == "__main__":
 					S=real(E)**2#0.5*numpy.cross(E.T,conjugate(B.T))
 					P[i,j]=sum(S)
 			print('%2.1f/100'%((k+1)/nt*100))
+			calcTime=time.time()-startTime
 			#Radiation diagram
-			#pcolormesh(x,z,P[:,:].T, vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
-			#pcolor(x,z,P[:,:].T, vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
 			pcolor(x,z,np.log10(P[:,:].T),vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
 			#pcolor(x,z,P[:,:].T, cmap='hot')
 			colorbar()
@@ -246,6 +135,7 @@ if __name__ == "__main__":
 			ylabel(r'$z/$m')
 			title(r'$t=%2.2f$ ns'%(t[k]/1e-9))
 			print('Saving frame' + fname)
+			print('Calculation Time: ' + time.strftime("%H:%M:%S", time.gmtime(calcTime)))
 			fig.savefig(path + '\\' + fname +'.png',bbox = 'tight')
 			clf()
 			magData = P[:,:].T #save magnitude of electric field
@@ -253,6 +143,7 @@ if __name__ == "__main__":
 			numpy.savetxt(path + "\\magData" + fname + ".csv",magData,delimiter = ',', fmt = "%s")
 			numpy.savetxt(path + "\\phaseData" + fname + ".csv",phaseData,delimiter = ',', fmt = "%s")
 	else: #single time sample
+		startTime = time.time()
 		fig = figure(num=1,figsize=(10,6),dpi=300)
 		#for k in range(nt): #t[k] is the time sample
 		P=numpy.zeros((nx,nz))
@@ -264,11 +155,8 @@ if __name__ == "__main__":
 				S=real(E)**2#0.5*numpy.cross(E.T,conjugate(B.T))
 				P[i,j]=sum(S)
 				phipArr[i,j] = real(phip) #save phase info into array
+		calcTime=time.time()-startTime
 		#Radiation diagram
-		#pcolormesh(x,z,P[:,:].T, vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
-		#pcolor(x,z,P[:,:].T, vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
-		#pcolor(x,z,np.log10(P[:,:].T),vmin=np.amin(P), vmax=np.amax(P), cmap='hot')
-		#pcolor(x,z,np.log10(P[:,:].T), cmap='hot')
 		pcolor(x,z,P[:,:].T, cmap='hot')
 		colorbar()
 		fname = 'EfieldPlot' + str(observationTime) + 'ns'
@@ -280,6 +168,7 @@ if __name__ == "__main__":
 		ylabel(r'$z/$m')
 		title('Electric Field Plot at ' + str(observationTime) + 'ns')
 		print('Saving frame' + fname)
+		print('Calculation Time: ' + time.strftime("%H:%M:%S", time.gmtime(calcTime)))
 		fig.savefig(path + '\\' + fname +'.png',bbox = 'tight')
 		clf()
 		numpy.savetxt(path + "\\magData.csv",P[:,:].T,delimiter = ',', fmt = "%s") #save magnitude of electric field
